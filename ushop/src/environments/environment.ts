324eb951a58328ceabda35d8f2bf19251301d9b8
Charles Burch <georgia_beast89@yahoo.com>
2019-06-07 19:33:46 -0500

    installed angular firebase and setup inital config

--- ushop/src/environments/environment.ts
+++ ushop/src/environments/environment.ts
@@ -4,5 +4,14 @@
 // The list of which env maps to which file can be found in `.angular-cli.json`.
 
 export const environment = {
-  production: false
+    production: true,
+   firebase: {
+      apiKey: 'AIzaSyCb7Ug7Q6XzeH_mO2TwWDXDebK8Vl1qakI',
+      authDomain: 'ushop-52264.firebaseapp.com',
+      databaseURL: 'https://ushop-52264.firebaseio.com',
+      projectId: 'ushop-52264',
+      storageBucket: 'ushop-52264.appspot.com',
+      messagingSenderId: '339588712653',
+      appId: '1:339588712653:web:08bcb0a77efc2e39'
+   }
 };
