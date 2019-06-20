2ce0fc2ce46452742531f7c0183ea11308d627f3
Charles Burch <georgia_beast89@yahoo.com>
2019-06-20 16:53:47 -0500

    show and hide the admin links

--- ushop/src/app/auth.service.ts
+++ ushop/src/app/auth.service.ts
@@ -1,22 +1,30 @@
+import { UserService } from './user.service';
+import { AppUser } from './models/app-user';
 import { ActivatedRoute } from '@angular/router';
 import { Observable } from 'rxjs/Observable';
+import { of } from 'rxjs/observable/of';
 import { AngularFireAuth } from 'angularfire2/auth';
 import { Injectable } from '@angular/core';
+import 'rxjs/add/operator/switchMap';
 import * as firebase from 'firebase';
 
 @Injectable()
 export class AuthService {
   user$: Observable<firebase.User>;
 
-  constructor(private afAuth: AngularFireAuth, private route: ActivatedRoute) {
+  constructor(
+    private userService: UserService,
+    private afAuth: AngularFireAuth,
+    private route: ActivatedRoute) {
     this.user$ = afAuth.authState;
+    console.log('called auth constructor');
   }
 
   login() {
     // returnUrl gets the query params of the last route you tried to access
     // if there are params go to that route else go home
     // note: if there are query params that means user tried to get into unautorized route else navigate to home page
-    let returnUrl = this.route.snapshot.queryParamMap.get('returnUrl') || '/';
+    const returnUrl = this.route.snapshot.queryParamMap.get('returnUrl') || '/';
     localStorage.setItem('returnUrl', returnUrl);
 
     this.afAuth.auth.signInWithRedirect(new firebase.auth.GoogleAuthProvider());
@@ -24,5 +32,16 @@ export class AuthService {
 
   logout() {
     this.afAuth.auth.signOut();
+  }
+  // this is the user from the database
+  // get appUser$(): Observable<AppUser> {
+  //   return this.user$
+  //     .switchMap(user => this.userService.get(user.uid));
+  // }
+
+  get appUser$(): Observable<AppUser> {
+    return this.user$
+      .switchMap(user => (user) ? this.userService.get(user.uid) : of<AppUser>(null)
+      );
   }
 }
--- ushop/src/app/bs-navbar/bs-navbar.component.ts
+++ ushop/src/app/bs-navbar/bs-navbar.component.ts
@@ -1,13 +1,18 @@
+import { AppUser } from './../models/app-user';
 import { AuthService } from './../auth.service';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
+  // tslint:disable-next-line:component-selector
   selector: 'bs-navbar',
   templateUrl: './bs-navbar.component.html',
   styleUrls: ['./bs-navbar.component.css']
 })
 export class BsNavbarComponent {
-  constructor(public auth: AuthService) {
+  appUser: AppUser;
+
+  constructor(private auth: AuthService) {
+    auth.appUser$.subscribe(appUser => this.appUser = appUser);
   }
 
   logout() {
