dbc3ba0420d9409527436b2cd668ef2309d2aabf
Charles Burch <georgia_beast89@yahoo.com>
2019-06-21 23:49:58 -0500

    populated the categories dropdown list

--- ushop/src/app/admin/product-form/product-form.component.ts
+++ ushop/src/app/admin/product-form/product-form.component.ts
@@ -1,15 +1,19 @@
+import { CategoryService } from './../../category.service';
 import { Component, OnInit } from '@angular/core';
 
-  @Component({
-   selector: 'app-product-form',
-   templateUrl: './product-form.component.html',
-   styleUrls: ['./product-form.component.css']
- })
- export class ProductFormComponent implements OnInit {
+@Component({
+  selector: 'app-product-form',
+  templateUrl: './product-form.component.html',
+  styleUrls: ['./product-form.component.css']
+})
+export class ProductFormComponent implements OnInit {
+  categories$;
 
-    constructor() { }
-
-    ngOnInit() {
-   }
-
+  constructor(categoryService: CategoryService) {
+    this.categories$ = categoryService.getCategories();
   }
+
+  ngOnInit() {
+  }
+
+}
